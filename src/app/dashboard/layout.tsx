edad6905cb22714f52eeb7daf895a4a35ab9fbef
prayag <prayag129787@gmail.com>
2024-03-30 22:59:27 +0530

    side bar added

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,8 +1,67 @@
+import {
+  DashboardNav,
+  NavItem,
+} from "@/components/ui/dashboard-nav/dashboard-nav";
+import { cn } from "@/lib/utils";
+export const navItems: NavItem[] = [
+  {
+    title: "Dashboard",
+    href: "/dashboard",
+    icon: "dashboard",
+    label: "Dashboard",
+  },
+  {
+    title: "User",
+    href: "/dashboard/user",
+    icon: "user",
+    label: "user",
+  },
+  {
+    title: "Employee",
+    href: "/dashboard/employee",
+    icon: "employee",
+    label: "employee",
+  },
+  {
+    title: "Profile",
+    href: "/dashboard/profile",
+    icon: "profile",
+    label: "profile",
+  },
+  {
+    title: "Kanban",
+    href: "/dashboard/kanban",
+    icon: "kanban",
+    label: "kanban",
+  },
+  {
+    title: "Login",
+    href: "/",
+    icon: "login",
+    label: "login",
+  },
+];
+
 function LayoutDashboard({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className="flex h-screen overflow-hidden">
-        <h1>tis layout</h1>
+        <nav
+          className={cn(
+            `relative hidden h-screen border-r pt-16 lg:block w-72`
+          )}
+        >
+          <div className="space-y-4 py-4">
+            <div className="px-3 py-2">
+              <div className="space-y-1">
+                <h2 className="mb-2 px-4 text-xl font-semibold tracking-tight">
+                  Overview
+                </h2>
+                <DashboardNav items={navItems} />
+              </div>
+            </div>
+          </div>
+        </nav>
         <main className="w-full pt-16">{children}</main>
       </div>
     </>
